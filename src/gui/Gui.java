e491ec9b3273e42ac30136ec04435ffd895d2050
Nathan <Nathan@nichols-ve505-0268.apn.wlan.upenn.edu>
2018-12-02 17:54:54 -0500

    Added Buttons

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -4,14 +4,39 @@ package gui;
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author Nathan
  */
 public class Gui extends JFrame
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	// Height and width of window
 	int WIDTH = 1280;
 	int HEIGHT = 720;
+	
+	// Colors of various elements
+	Color backgroundColor = new Color(40, 120, 20);
+	Color colorButton = new Color(250, 250, 250);
+	
+	
+	// Buttons
+	Button hitButton = new Button();
+	Button stayButton = new Button();
+	Button yesButton = new Button();
+	Button noButton = new Button();
+	
+	
+	// Fonts
+	Font buttonFont = new Font("Times New Roman", Font.PLAIN, 40);
+	
 	
 	public Gui()
 	{
@@ -21,8 +46,134 @@ public class Gui extends JFrame
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		Board board = new Board();
+		this.setContentPane(board);
+		//this.setLayout(null);
+		
+		
+		// Hit button
+		ActionHit aHit = new ActionHit();
+		hitButton.addActionListener(aHit);
+		
+		hitButton.setBounds(400, 400, 120, 80);
+		hitButton.setBackground(colorButton);
+		hitButton.setFont(buttonFont);
+		//hitButton.setText("HIT");
+		
+		board.add(hitButton);
+		
+		
+		
+		// Stay button
+		ActionStay aStay = new ActionStay();
+		stayButton.addActionListener(aStay);
+		
+		stayButton.setBounds(600, 400, 120, 80);
+		stayButton.setBackground(colorButton);
+		stayButton.setFont(buttonFont);
+		//stayButton.setText("STAY");
+		
+		board.add(stayButton);
+		
+		
+		
+		// Yes Button
+		ActionYes aYes = new ActionYes();
+		yesButton.addActionListener(aYes);
+		
+		yesButton.setBounds(400, 600, 120, 80);
+		yesButton.setBackground(colorButton);
+		yesButton.setFont(buttonFont);
+		//yesButton.setText("YES");
+		
+		board.add(yesButton);
+		
+		
+		//
+		ActionNo aNo = new ActionNo();
+		noButton.addActionListener(aNo);
+		
+		noButton.setBounds(600, 600, 120, 80);
+		noButton.setBackground(colorButton);
+		noButton.setFont(buttonFont);
+		//hitButton.setText("NO");
+		
+		board.add(noButton);
+		
 		
 	}
+	
+	
+	public class Board extends JPanel
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		 
+		public void paintComponent(Graphics graphic)
+		{
+			graphic.setColor(backgroundColor);
+			graphic.fillRect(0, 0, WIDTH, HEIGHT);
+			
+			
+		}
+		
+	}
+	
+	
+	public class ActionHit implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("Hit button clicked.");
+			
+		}
+		
+	}
+	
+	
+	public class ActionStay implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("Stay button clicked.");
+			
+		}
+		
+	}
+	
+	
+	public class ActionYes implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("Yes button clicked.");
+			
+		}
+		
+	}
+	
+	
+	public class ActionNo implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.out.println("No button clicked.");
+			
+		}
+		
+	}
+	
 	
 	
 }
